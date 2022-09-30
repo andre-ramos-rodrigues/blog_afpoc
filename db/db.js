b22ac44695271ce7f1a59ef82924e81e805ad1c6
Andre <andreramos@id.uff.br>
2022-09-30 15:15:02 -0300

    ..

--- db/db.js
+++ db/db.js
@@ -1,4 +1,4 @@
-import mysql from "mysql"
+import mysql from "mysql2"
 
 export const db = mysql.createConnection({
   host: 'us-cdbr-east-06.cleardb.net',
@@ -21,29 +21,4 @@ export const db = mysql.createConnection({
   database: "blog"
 })
 */
-
-var connection;
-
-function handleDisconnect() {
-  connection = mysql.createConnection(db); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
 
