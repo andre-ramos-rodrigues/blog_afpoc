7cd28b564a12be3bd9485bd8a14f49c02b76adf7
Andre <andreramos@id.uff.br>
2022-09-30 11:04:19 -0300

    changing the db connection

--- db/db.js
+++ db/db.js
@@ -1,8 +1,25 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
+  host: 'us-cdbr-east-06.cleardb.net',
+  user: 'bd71a4d05fd9b2',
+  password: "af7adfbe",
+  database: "heroku_a2c3e52abb14414"
+})
+
+// go to heroku >> resources >> add-ons and add mysql clearDB
+// change the db config above inserting the config bellow:
+//               user     password            host                            database
+// mysql://bd71a4d05fd9b2:af7adfbe@us-cdbr-east-06.cleardb.net/heroku_a2c3e52abb14414?reconnect=true
+
+/*
+original db config
+export const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: "1181993",
   database: "blog"
 })
+*/
+
+
