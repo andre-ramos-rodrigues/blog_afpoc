abb700b03f7045362744f023ec228c0221b449f9
Andre <andreramos@id.uff.br>
2022-10-03 11:49:31 -0300

    .

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://brilliant-palmier-9298e8.netlify.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://brilliant-palmier-9298e8.netlify.app/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
