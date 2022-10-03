a582dc932ba60e789ed8a7111fd18677cc848b3a
Andre <andreramos@id.uff.br>
2022-10-03 15:24:32 -0300

    access crontrol

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, DELETE, OPTIONS"
   );
+  res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('content-type', 'application/json');
   next();
 });
