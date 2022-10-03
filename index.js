693b33885e2539101362e29af5020b774fc907cd
Andre <andreramos@id.uff.br>
2022-10-03 18:07:54 -0300

    .

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ app.use("/api/users", usersRoute)
 
 // headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://brilliant-palmier-9298e8.netlify.app"); // the link of my front-end app on Netlify
-  res.setHeader("Access-Control-Allow-Origin", "https://api.cloudinary.com/v1_1/dmqnk9v0d/auto/upload")
+  res.setHeader("Access-Control-Allow-Origin", "https://brilliant-palmier-9298e8.netlify.app" && "https://api.cloudinary.com/v1_1/dmqnk9v0d/auto/upload" && "http://localhost:3000")
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
