50cf32733b5b961a56215ae3b48a412a463e536f
Andre <andreramos@id.uff.br>
2022-10-04 11:46:40 -0300

    .

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ const app = express()
 app.use(express.json())
 
 // resolving cors
+const origin = "https://afpoc-blog.herokuapp.com"
 app.use(
   cors({
-    origin: "*"
+    origin: origin,
+    credentials: true,
   })
 )
 
